c60e8e66c6fa5e6e60229eb973af4112a5a793ba
whatever071917 <whatever071197@gmail.com>
2024-12-24 00:48:56 +0700

    du ma nextjs

--- sample-blog/src/app/components/menu.tsx
+++
@@ -1,5 +0,0 @@
-export default function MenuBar() {
-  return (
-    <div className={'bg-gray-700 h-10'}>Menu</div>
-  )
-}
--- sample-blog/src/app/page.tsx
+++ sample-blog/src/app/page.tsx
@@ -1,10 +1,69 @@
-import Image from "next/image";
-import Homepage from "./components/homepage";
+"use client";
+
+import {useEffect, useState} from "react";
+import PostComponent from "@/app/(routers)/posts/page";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from "@/components/ui/pagination";
+import {getPosts} from "@/app/services/fetch-data";
+import {Post} from "@/app/models/data.model";
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSizes, setPageSizes] = useState(9);
+  const [data, setData] = useState<Post[]>([]);
+  const [paginatedData, setPaginatedData] = useState<Post[][]>([]);
+
+  useEffect(() => {
+    getPosts().then(data => setData(data));
+    const paginateData = []
+    for (let i = 0; i < data.length; i += pageSizes) {
+      paginateData.push(data.slice(i, i + pageSizes))
+    }
+    setPaginatedData(paginateData);
+  }, [data])
+
   return (
-      <div className={'h-full bg-zinc-800'}>
-        <Homepage />
+    <>
+      <div className="flex flex-wrap h-5/6 justify-center items-center bg-zinc-800">
+        <PostComponent posts={paginatedData[currentPage]}/>
       </div>
+      <Pagination className="text-white bg-zinc-800 h-1/6">
+        <PaginationContent>
+          <PaginationItem className={'p-10'}>
+            <PaginationPrevious
+              href="#"
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 0))}
+            />
+          </PaginationItem>
+          {paginatedData.map((_, index) => (
+            <PaginationItem key={index} className={'p-2'}>
+              <PaginationLink
+                className={`${index + 1 === currentPage} ? 'text-red-400 text-2xl' : ''`}
+                href="#"
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem className={'p-10'}>
+            <PaginationNext
+              href="#"
+              onClick={() =>
+                setCurrentPage((prev) =>
+                  Math.min(prev + 1, paginatedData.length - 1)
+                )
+              }
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </>
   );
 }
